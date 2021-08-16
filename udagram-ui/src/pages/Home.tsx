6f7e29c591e4aaab38866a9a8ed8cfe895f81a5a
acald <acaldwell@phoenixveritas.com>
2021-08-16 14:11:00 -0500

    Added a menu with a title

--- udagram-ui/src/pages/Home.tsx
+++ udagram-ui/src/pages/Home.tsx
@@ -1,13 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonApp, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import MenuBar from '../components/MenuBar/MenuBar';
 import './Home.css';
 
+/*
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Udagram</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -21,5 +22,38 @@ const Home: React.FC = () => {
     </IonPage>
   );
 };
+*/
 
+const Home: React.FC = () => {
+  return (
+    <IonApp>
+      <IonSplitPane>
+        <IonMenu>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>
+                Menu
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonList>
+              <IonMenuToggle>
+                <IonItem>
+                  <IonIcon slot="start"></IonIcon>
+                  <IonLabel>
+                    title of icon
+                  </IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            </IonList>
+          </IonContent>
+        </IonMenu>
+        <IonContent>
+          <MenuBar />
+        </IonContent>
+      </IonSplitPane>
+    </IonApp>
+  )
+}
 export default Home;
